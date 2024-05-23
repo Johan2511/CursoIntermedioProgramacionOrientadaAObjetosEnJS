d550a70d0232f73d6021cfc5af62f32d6260414b
Johan2511 <johanpenuelah@gmail.com>
2024-05-22 21:54:54 -0500

    Uso de Object.defineProperty

--- main.js
+++ main.js
@@ -13,11 +13,42 @@ const juan = {
 // console.log(Object.getOwnPropertyNames((juan))); 
 // console.log(Object.entries((juan)));
 
-console.log(Object.getOwnPropertyDescriptors(juan));
 
-Object.defineProperty(juan, "pruebaNASA", {
-    value:"extraterrestres",
-    enumerable: true,
-    writable: true,
-    configurable: true
-} );
+// Object.defineProperty(juan, "", {
+//     value:"",
+//     enumerable: true,
+//     writable: true,
+//     configurable: true,
+// } );
+// Object.defineProperty(juan, "navigator", {
+//     value:"Chrome",
+//     enumerable: false,
+//     writable: true,
+//     configurable: true,
+// } );
+
+// Object.defineProperty(juan, "editor", {
+//     value:"VsCode",
+//     enumerable: false,
+//     writable: false,
+//     configurable: true,
+// } );
+
+// Object.defineProperty(juan, "terminal", {
+//     value:"WSL",
+//     enumerable: true,
+//     writable: true,
+//     configurable: false,
+// } );
+
+// Object.defineProperty(juan, "pruebaNASA", {
+//     value:"extraterrestres",
+//     enumerable: false,
+//     writable: false,
+//     configurable: false,
+// } );
+
+// Object.seal(juan); // Ayuda a que las prpiedades se puedan borrar con el configurable en false
+Object.freeze(juan); // configurable en false y writable en false no se puede borrar ni sobreescribir
+
+console.log(Object.getOwnPropertyDescriptors(juan));
