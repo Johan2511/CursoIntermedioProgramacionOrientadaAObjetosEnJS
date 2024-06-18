ccecaa1ebc695c4fdb8466d90d4c5812d3de6ad3
Johan2511 <johanpenuelah@gmail.com>
2024-06-17 22:11:02 -0500

    Uso de de duck typing en javascript

--- main2.js
+++ main2.js
@@ -15,8 +15,98 @@
 // const obj3 = Object.assign({}, obj1) ;
 // const obj4 = Object.create(obj1) ;
 
+// Abstracción con objetos literales y deep copy
+
+function isObject(subject) {
+    return typeof subject == "object";
+}
+function isArray(subject) {
+    return Array.isArray(subject)
+}
+
+function deepCopy(subject) {
+    let copySubject;
+ 
+    const subjectIsObject = isObject(subject);
+    const subjectIsArray = isArray(subject)
+ 
+    if (subjectIsArray) {
+     copySubject = [];
+    } else if (subjectIsObject) {
+     copySubject = {};
+    } else {
+     return subject;
+    }
+ 
+    for (key in subject) {
+     const keyIsObject = isObject(subject[key]);
+     if (keyIsObject) {
+         // copySubject[key] = deepCopy(subject[key]);
+     } else {
+         if (subjectIsArray) {
+             copySubject.push(subject[key]);
+         } else {
+             copySubject[key] = subject[key];
+         }
+     }
+    }
+ 
+    return copySubject; 
+ }
+
+
+ const studentBase = {
+    name: undefined,
+    email: undefined,
+    age: undefined,
+    approveCourse: undefined,
+    learningPaths: undefined,
+    socialMedia: {
+        twitter: undefined,
+        instagram: undefined,
+        facebook: undefined,
+    }
+ };
+
+ const Johana = deepCopy(studentBase);
+ Object.seal(Johana);
+//  Object.isSealed(Johana);
+// Object.isFrozen(Johana);
+    Johana.name ='Jonanita'
+
 function requiredParam(param) {
     throw new Error(param + " es obligatio");
+}
+
+function createLearningPath({
+    name = requiredParam(name),
+    courses: [],
+}){
+    const private = {
+        "_name": name,
+        "_courses": courses,
+        
+    };
+
+    const public = {
+        get name() {
+            return private['_name'];
+        },
+
+        set name(newName) {
+            if (newName.length != 0) {
+                private['_name'] = newName;
+            } else {
+                console.warn("Tu nombre debe tener al menos 1 carácter");
+            }
+        },
+
+        get courses() {
+            return private['_courses'];
+        },
+
+    };
+    return public;
 }
 
 function createStudents({
@@ -32,13 +122,13 @@ function createStudents({
 
     const private = {
         "_name": name,
+        "_learningPaths": learningPaths
     };
 
     const public = {
         email,
         age,
         approvedCourse,
-        learningPaths,
         socialMedia: {
             twitter,
             instagram,
@@ -55,7 +145,32 @@ function createStudents({
             } else {
                 console.warn("Tu nombre debe tener al menos 1 carácter");
             }
-        }
+        },
+        get learningPaths() {
+            return private['_learningPaths'];
+        },
+
+        set learningPaths(newLP) {
+
+            if (!newLP.name){
+                console.warn("Tu LP no tiene la propiedad name");
+                return;
+            } 
+
+            if (!newLP.courses) {
+                console.warn("Tu LP no tiene Courses");
+                return;
+            }
+            if (!isArray(newLP.courses)) {
+                console.warn("Tu LP no es una lista (*de cursos)");
+                return;
+            }
+                
+
+            private["_learningPaths"] = newLP;
+
+
+        },
     // readName() {
     //     return private['_name'];
     //     },
@@ -90,3 +205,5 @@ const juaninto = createStudents({
     facebook: 'facebook.com',
     instagram: 'instagram.com',
 }) ;
+
+juaninto.learningPaths = "Nueva ruta de aprendizaje"
